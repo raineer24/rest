46677f96ac9b05b0c51e438c268b51489c84b1b7
unknown <ra@gmail.com>
2017-08-19 18:21:06 -0700

    getting a single item

--- app.js
+++ app.js
@@ -12,7 +12,13 @@ var bookRouter = express.Router();
 
 bookRouter.route('/Books')
 	.get(function(req, res){
-		Book.find(function(err, books) {
+
+		var query = {};
+		if(req.query.genre)
+		{
+			query.genre = req.query.genre;
+		}
+		Book.find(query, function(err, books) {
 			if(err)
 				res.status(500).send(err);
 			else
@@ -20,6 +26,17 @@ bookRouter.route('/Books')
 		});
 	});
 
+bookRouter.route('/Books/:bookId')	
+	.get(function(req, res){
+	Book.findById(req.params.bookId, function(err, book) {
+			if(err)
+				res.status(500).send(err);
+			else
+				res.json(book);
+		});
+	});
+
+//endshere
 app.use('/api', bookRouter);	
 
 app.get('/', function(req, res) {
